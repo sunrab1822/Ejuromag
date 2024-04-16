496855ea1ee41745980fa1d8e72aeebae2453f79
Szergely <117809877+SzergelyHUN@users.noreply.github.com>
2024-04-16 10:25:33 +0200

    Update AppNav2.vue

--- src/components/AppNav2.vue
+++ src/components/AppNav2.vue
@@ -4,22 +4,22 @@
               <div id="navbarSupportedContent" class="collapse navbar-collapse">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link" data-cy="navbar2-laptops-link" href="/laptopok">Laptopok</a>
+                    <router-link class="nav-link" data-cy="navbar2-laptops-link" to="/laptopok">Laptopok</router-link>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" data-cy="navbar2-gamerPcs-link" href="/gamer_szamitogepek">Gamer számítógépek</a>
+                    <router-link class="nav-link" data-cy="navbar2-gamerPcs-link" to="/gamer_szamitogepek">Gamer számítógépek</router-link>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" data-cy="navbar2-officePcs-link" href="/irodai_szamitogepek">Irodai számítógépek</a>
+                    <router-link class="nav-link" data-cy="navbar2-officePcs-link" to="/irodai_szamitogepek">Irodai számítógépek</router-link>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" data-cy="navbar2-consoles-link" href="/konzolok">Konzolok</a>
+                    <router-link class="nav-link" data-cy="navbar2-consoles-link" to="/konzolok">Konzolok</router-link>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" data-cy="navbar2-phones-link" href="/telefonok">Telefonok</a>
+                    <router-link class="nav-link" data-cy="navbar2-phones-link" to="/telefonok">Telefonok</router-link>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" data-cy="navbar2-tablets-link" href="/tabletek">Tabletek</a>
+                    <router-link class="nav-link" data-cy="navbar2-tablets-link" to="/tabletek">Tabletek</router-link>
                   </li>
 
                 </ul>
